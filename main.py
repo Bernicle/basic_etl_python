322bd0bf022ad8e28a050e005e34434fae561777
Bernard Ponce <poncebernard123@gmail.com>
2025-06-21 22:09:18 +0800

    Create basic Main Pipeline for ETL

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Simple Template to Perform ETL
+
+def run_etl_pipeline():
+    # Step 1 Extract
+
+    # Step 2 Transform
+    
+    # Step 3 Load
+    
+    pass
+
+
+if __name__ = "__main__":
+    run_etl_pipeline()
